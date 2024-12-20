8109c7fd3b9b91f22db96d872551698a81a50be9
x <x>
2024-12-20 09:46:51 +0200

    start-dog

--- website/pages/signup.ts
+++ website/pages/signup.ts
@@ -1,9 +1,9 @@
 import { send } from "../utilities";
 
 let signupButton = document.querySelector("#signupButton") as HTMLButtonElement;
-
 let usernameInput = document.querySelector("#usernameInput") as HTMLInputElement;
 let passwordInput = document.querySelector("#passwordInput") as HTMLInputElement;
+
 signupButton.onclick = async function() {
     await send("signup", [usernameInput.value, passwordInput.value]);
     alert("User registered successfully!");
